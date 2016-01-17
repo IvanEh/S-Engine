60e1e34ae0c8fee8b0cc178e205dd578e9923c42
IvanEh <ivanehreshi@gmail.com>
2016-01-17 16:26:02 +0200

    Add time mesuarement facility in utility.hpp

---
+++ src/utils.cpp
@@ -0,0 +1,8 @@
+#include "headers/utils.hpp"
+
+int timems() {
+  auto timeSinceEpoch = std::chrono::system_clock::now().time_since_epoch();
+  auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(timeSinceEpoch);
+  return ms.count();
+}
+
