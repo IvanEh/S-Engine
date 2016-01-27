837e3b0034c375be07a702a11faf7b00c466e4d2
IvanEh <ivanehreshi@gmail.com>
2016-01-27 13:29:52 +0200

    Remove ambigious Context class

--- src/include/app/Events.hpp
+++ src/include/app/Events.hpp
@@ -4,14 +4,8 @@
 
 namespace s_engine {
 namespace app {
-
-class Context {};  
   
 class Event {
-private:
-  const Context* context = nullptr;
-public:
-  const Context* GetContext() const;
 };
 
 
