724e1c48cfdcd8ef05b5e963a50a445b6bc8af1a
IvanEh <ivanehreshi@gmail.com>
2016-02-03 22:52:59 +0200

    Add scene

--- src/include/graphics/Scene.hpp
+++ src/include/graphics/Scene.hpp
@@ -1,1 +1,42 @@
- 
+#pragma once
+
+#include <list>
+
+#include "app/Entity.hpp"
+
+using namespace std;
+
+
+class Scene {
+private: /* fields */
+    /**
+     * For better performance drawable entities stored seperately 
+     * from other entities
+     */
+    list<DrawableEntities> drawables;
+    
+    /**
+     * Non-drawable entities manage the logics of the scene.
+     */
+    list<Entity> managers;
+    
+    /**
+     * Scene camera
+     */
+    Camera camera;
+public: /* methods */
+    /**
+     * Called when a particular scene prepared to execute
+     */
+    virtual void OnStart();
+    
+    /**
+     * Called every frame
+     */
+    virtual void OnUpdate(long deltaTime);
+    
+    /**
+     * Draw the scene
+     */
+    virtual void OnDraw();
+};
