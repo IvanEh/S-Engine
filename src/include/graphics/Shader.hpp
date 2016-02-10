f085b9254d60dd6da3ed178a9faf84176e553657
IvanEh <ivanehreshi@gmail.com>
2016-02-10 19:10:13 +0200

    Add Shader.hpp

--- src/include/graphics/Shader.hpp
+++ src/include/graphics/Shader.hpp
@@ -1,1 +1,33 @@
- 
+#pragma once
+
+#include <string>
+
+#include <GL/glew.h>
+
+#include "app/Resources.hpp"
+
+using namespace std;
+
+using namespace s_engine::app;
+
+namespace s_engine {
+namespace graphics {
+
+//using namespace app;
+
+class Shader: public Resource {
+protected:
+    string vertexSource;
+    string fragmentSource;
+    GLuint vertexShaderId;
+    GLuint fragmentShaderId;
+    GLuint shaderProgramId;
+public:
+    Shader(const string& _fragmentSource, const string& _vertexSource);
+    
+    void Use();
+};
+
+
+}
+}
